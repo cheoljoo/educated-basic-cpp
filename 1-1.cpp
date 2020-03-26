2355eae2e37857912cc3095e0b3514413c300c48
Charles.Lee <cheoljoo@gmail.com>
2020-03-27 00:34:56 +0900

    -

---
+++ 1-1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class String {
+private:
+    char* buff;
+    int size;
+    int* ref;
+public:
+    String(){  
+        cout << "String()" << endl;
+    }
+    ~String(){ 
+        cout << "~String()" << endl;  
+        delete buff; 
+    }
+
+};
+
+int main() {
+// 1단계. 생성자, 소멸자, 화면 출력
+    String s1 = "hello";
+    exit(1);
+    cout << s1 << endl;
+// 2단계. 복사 생성자
+    String s2 = s1;
+    cout << s2 << endl;
+// 3단계. 대입연산자
+    String s3 = "student";
+    s3 = s1;
+    cout << s3 << endl;
+// 4단계. 자신과의 대입
+    s1 = s1;
+    cout << s1 << endl;
+// 5단계. 디폴트 생성자
+String s4;
+    cout << s4 << endl;
+}
